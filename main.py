7ec3df3bcffc667410807fbce30c08b2810afd7f
DADE-222 <fernandes.antunes.alexis@gmail.com>
2025-04-08 16:30:28 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+## Résolution des problèmes
+- **Module introuvable** :
+Vérifiez que tous les sous-dossiers existent dans `C:\Program Files (x86)\whisperUX`.
+- **FFmpeg non trouvé** :
+Assurez-vous que les fichiers binaires de FFmpeg sont dans le dossier `ffmpeg-7.1`.
+
+## Contact
+Pour toute assistance, contactez [votre adresse email ou site web].
